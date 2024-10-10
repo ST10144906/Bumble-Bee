a255a5081d986de42a84cda4ec214966d0d22ff2
MxKale <brfireblaze@gmail.com>
2024-10-10 17:49:39 +0200

    Updated Dashboards

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,13 +1,41 @@
 ﻿using BumbleBeeWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BumbleBeeWebApp.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly AuthService _authService;
+
+
+        public DashboardController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        /*
+        public async Task<IActionResult> Index()
+        {
+            var userEmail = User.Identity.Name;
+            var userRecord = await FirebaseAuth.DefaultInstance.GetUserByEmailAsync(userEmail);
+            var userId = userRecord.Uid;
+
+            var userDoc = await _authService.GetUserDocumentAsync(userId);
+            string userType = userDoc.ContainsField("Type") ? userDoc.GetValue<string>("Type") : "Unknown";
+
+            var model = new DashboardViewModel
+            {
+                UserRole = userType
+            };
+
+            return View("Dashboard", model);
+        }
+        */
+
         public ActionResult Index()
         {
-            string role = GetUserRole(User.Identity.Name);
+            string role = "Company";
 
             var model = new DashboardViewModel
             {
@@ -20,7 +48,7 @@ namespace BumbleBeeWebApp.Controllers
         private string GetUserRole(string username)
         {
             // Placeholder for getting user role, replace with actual logic
-            return "Admin"; // Default to Admin for testing
+            return "Company"; // Default to Admin for testing
         }
     }
 }
