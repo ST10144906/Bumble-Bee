5dd6659b5694ac4b5d523a5fabfa043ab031840d
Ryanct12 <st10063915@vcconnect.edu.za>
2024-11-25 00:25:57 +0200

    Download Button Functionality

--- Services/StorageService.cs
+++ Services/StorageService.cs
@@ -21,17 +21,35 @@ public class StorageService
         return fileUrl;
     }
 
-    public async Task<byte[]> DownloadFileAsync(string objectName)
- {
-     var bucketName = "bumble-bee-foundation.appspot.com";
+    public async Task<byte[]> DownloadFileAsync(string fileUrl)
+    {
+        try
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Console.WriteLine($"Attempting to download file from: {fileUrl}");
 
-     using (var memoryStream = new MemoryStream())
-     {
-         await _storageClient.DownloadObjectAsync(bucketName, objectName, memoryStream);
-         Console.WriteLine($"Downloaded {objectName}.");
-         return memoryStream.ToArray();
-     }
- }
+                // Make a GET request to the file URL
+                var response = await httpClient.GetAsync("https://storage.googleapis.com/"+fileUrl);
+                response.EnsureSuccessStatusCode();
+
+                // Read the file contents as a byte array
+                var fileBytes = await response.Content.ReadAsByteArrayAsync();
+                Console.WriteLine($"File downloaded successfully from: {fileUrl}");
+                return fileBytes;
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"HTTP error downloading file: {ex.Message}");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"General error downloading file: {ex.Message}");
+            throw;
+        }
+    }
 
 }
 
