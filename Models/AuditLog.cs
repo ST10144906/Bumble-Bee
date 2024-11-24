6cefc89770e99369a2f88704ec8ddda42d664554
jordanfarrell88 <105391488+jordanfarrell88@users.noreply.github.com>
2024-11-24 15:09:32 +0200

    Audit Feature

---
+++ Models/AuditLog.cs
@@ -0,0 +1,14 @@
+﻿namespace BumbleBeeWebApp.Models
+{
+    public class AuditLog
+    {
+        public string AuditLogId { get; set; }
+        public string DonationId { get; set; }
+        public string ProjectName { get; set; }
+        public string UserId { get; set; }
+        public double Amount { get; set; }
+        public string PaymentType { get; set; }
+        public DateTime AuditedAt { get; set; }
+        public string Notes { get; set; }
+    }
+}
